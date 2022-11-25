612ccd4ca5edce57939bbb66d3c4f021a93eea31
Amari Cross <amaricross77@gmail.com>
2022-11-24 22:42:53 -0500

    testing 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from '/aos';
+import '/aos/dist/aos.css';
 import React,{useEffect} from "react";
 
 
